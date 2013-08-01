95993768010823d4d5c3f53ff5f0203e5ca95e90
Guo Yu <i@guoyu.me>
2013-08-01 20:00:37 +0800

    func zip done

--- index.js
+++ index.js
@@ -5,13 +5,22 @@
 // \___/\____/\____/_/|_|\___/_/     
 // 
 // a minimalism style version manager
+
 var optimist = require('optimist'),
     argv = optimist.argv,
-    watcher = require('./lib/watcher');
+    color = require('colors'),
+    zipper = require('./lib/zipper');
 
 // CLI
 exports.cli = function() {
     if (argv.w || argv.watch) {
-        console.log('works!')
+        console.log(color.yellow('zipping...!'));
+        var zip = new zipper( __dirname + '/demo.zip');
+        zip.create([{
+          name: 'demo.txt',
+          dir: __dirname + '/demo.txt'  
+        }],function(stat){
+            console.log('ok');
+        });
     }
 }
