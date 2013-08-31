15f2205d8758ccfa79e5344b7c6614cac7f470cf
Guo Yu <i@guoyu.me>
2013-09-01 03:07:43 +0800

    ship to 0.1.0

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # ![logo](http://ww4.sinaimg.cn/large/61ff0de3jw1e76l9veqwsj20190190sj.jpg) cooker ![npm](https://badge.fury.io/js/cooker.png)
 
-123
-
 a minimalism style version manager for static files.
 
 ## How to install
@@ -16,54 +14,16 @@ $ sudo npm install cooker -g
 ````
 // update version by youself
 $ cooker up README.md
+
+// update versions of a.md and b.js
+$ cooker up a.md b.js
+
+// update versions of all js files
+$ cooker up -f *.js
+
+// update versions of a.md and all css files in all dirs
+$ cooker up a.md -f **/*.css
 ````
-
-### Sample code
-
-now building...
-
-````javascript
-var cooker = require('cooker');
-
-// using cooker to watch psd changes
-cooker.watch('./psd',function(changes){
-  console.log(changes);
-});
-
-// watch file changes and created when new psd added.
-cooker.watch('./psd',function(changes){
-  // if new psd added to './psd'
-  if (changes.type == 'new') {
-    // using defalut zip config
-    cooker.zip('./psd',function(zipfile){
-      // zipfile should be 'psd_v0.0.x.zip'
-      console.log(zipfile)
-    })
-  }
-});
-````
-
-## API
-
-### cooker.watch(config,callback) 
-- `config` [Object] or [String] :
-  - `ingore`: [Array] : filetypes to ignore.
-  - `dir`: [String] : dir for watch
-- `callback` [Function]
-  - `err`
-  - `changes`: [Array] list every changes here.
- 
-### cooker.zip(config,callback)
-- `config` [Object] or [String]:
-  - `fileDir`: [String] dir wanna to zip
-  - `dist`: [String] `mv` zip file to selected dist dir
-  - `start`: [Number] : like `0.0.1` or `1.0.0`
-  - `divider`: [String] : divider that divide filename from version-stamps , e.g: "-" or "." or ("_" -> "filename_v0.1.0" )
-  - `afterfix` : [String] : string after version-stamp. e.g: "filename_v1.0.1_2013-20-20.zip"
-- `fileDir` [String]
-- `callback` [Function]
-  - `err`
-  - `pkg` zipped package dir
 
 ## Run unit-test (Mocha)
 
@@ -73,7 +33,3 @@ $ cd cooker
 $ npm install 
 $ npm test
 ````
-
-## Changelog
-
-- `0.0.1` logo added
--- index.js
+++ index.js
@@ -9,41 +9,52 @@
 
 var optimist = require('optimist'),
     argv = optimist.argv,
-    color = require('colors'),
-    version = require('./lib/version');
+    version = require('./lib/version'),
+    finder = require('glob'),
+    consoler = require('consoler');
 
 // CLI
 exports.cli = function() {
     var params = argv._;
-    // 手动升级版本，必须指定文件
+    // 手动升级版本
     if (params[0] == 'up') {
+        var files = [];
+        // 指定了文件
         if (params.length > 1) {
-            var files = params.slice(1);
-            // 读取执行命令当前目录下的文件版本表
-            console.log(color.yellow('[ Loading ] ') + '正在读取版本信息...');
+            files = params.slice(1);
+        }
+        // 寻找匹配的文件
+        if (argv.f) {
+            files = files.concat(finder.sync(argv.f));
+        }
+        if (params.length == 1 && !argv.f) {
+            files = finder.sync('**/*');
+        }
+        if (files.length > 0) {
+            consoler.log('loading', '正在读取版本信息...');
             version.read(function(exist, versions) {
                 if (exist) {
-                    console.log(color.yellow('[ Success ] ') + '版本信息读取成功...');
+                    consoler.log('success', '版本信息读取成功...')
                     // 升级版本
                     version.update(versions, files, function(err) {
                         if (!err) {
-                            console.log(color.green('[ Success ] ') + '新版本创建成功，备份完成');
+                            consoler.log('success', '新版本创建成功，备份完成')
                         } else {
-                            console.log(color.red('[ Error ] ') + '版本创建失败，详情如下：');
+                            consoler.log('error', '版本创建失败，详情如下：')
                             console.log(err);
                         }
                     });
                 } else {
                     // 创建一个新的版本清单
-                    console.log(color.yellow('[ Loading ] ') + '没有找到版本清单...');
+                    consoler.log('loading', '没有找到版本清单...')
                     version.createList(files, function(filelist) {
-                        console.log(color.green('[ Success ] ') + '版本清单创建成功，创建时间：' + new Date());
+                        consoler.log('success', '版本清单创建成功，创建时间：' + new Date())
                         // 保留一份老版本
                         version.backup(filelist, function(err) {
                             if (!err) {
-                                console.log(color.green('[ Success ] ') + filelist[0].name + '等文件的新版本创建成功，备份完成');
+                                consoler.log('success', filelist[0].name + filelist[0].type + ' 等文件的新版本创建成功，备份完成')
                             } else {
-                                console.log(color.red('[ Error ] ') + '版本创建失败，详情如下：');
+                                consoler.log('error', '版本创建失败，详情如下：')
                                 console.log(err);
                             }
                         });
@@ -51,10 +62,7 @@ exports.cli = function() {
                 }
             });
         } else {
-             console.log(color.red('[ Error ] ') + '请指定需要监控版本的文件名称');
+            consoler.log('error','没有找到任何匹配的文件，请指定文件');
         }
     }
 };
-
-exports.watch = require('./lib/watcher').watch;
-exports.zip = require('./lib/file').zip;
