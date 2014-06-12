a47c7701423e9ce3ef1ab95674d3185fa22868b3
Guo Yu <i@guoyu.me>
2014-06-12 16:10:05 +0800

    update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//                     __            
+//   _________  ____  / /_____  _____
+//  / ___/ __ \/ __ \/ //_/ _ \/ ___/
+// / /__/ /_/ / /_/ / ,< /  __/ /    
+// \___/\____/\____/_/|_|\___/_/     
+// 
+// @brief: a minimalism style version manager for static files
+// @author: [turing](http://guoyu.me)
+
+module.exports = require('./libs/cooker');
